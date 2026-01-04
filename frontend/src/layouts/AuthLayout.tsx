79c3bc559f99ebda7b76ee9ab8383a66de5c0d2c
Mark Buckle <markbuckle92@gmail.com>
2026-01-04 14:06:46 -0330

    Sidebar responsiveness tweaks

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -21,7 +21,7 @@ const AuthLayout: React.FC = () => {
       {/* Sidebar */}
       <aside 
         className={`${
-          sidebarCollapsed ? 'w-20' : 'w-64'
+          sidebarCollapsed ? 'w-17' : 'w-56'
         } bg-white border-r border-gray-200 transition-all duration-300 flex flex-col`}
       >
         {/* Logo */}
@@ -102,9 +102,9 @@ const AuthLayout: React.FC = () => {
           </div>
           
           <div className="flex items-center space-x-4 ml-6">
-            <button className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary-dark">
+            {/* <button className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-primary-dark">
               + New
-            </button>
+            </button> */}
             <button className="p-2 rounded-lg hover:bg-gray-100">
               <Bell className="w-5 h-5 text-gray-600" />
             </button>
